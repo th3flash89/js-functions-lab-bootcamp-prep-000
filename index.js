b3645a7d7090f91ca322e4a66c43c71ee357882f
Trevor LeRiche <hoodooman04@gmail.com>
2019-07-25 23:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown('Mother\'s Day','20') {
-  return `It's ${days}, days until ${holiday}!`
+function holidayCountdown('Mother\'s Day',20) {
+  return `It's ${days} days until ${holiday}!`
 }
